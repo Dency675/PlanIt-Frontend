4f54f9aac7229155f809de6bb9cd80231166712e
geevarghese2000 <geevarghesemvarghese@gmail.com>
2024-01-31 19:19:43 +0530

    added room creation page

---
+++ src/pages/RoomCreation/RoomCreation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateRoomForm from "../../components/RoomCreation/CreateRoomForm";
+
+const RoomCreation = () => {
+  return (
+    <div>
+      <CreateRoomForm />
+    </div>
+  );
+};
+
+export default RoomCreation;
