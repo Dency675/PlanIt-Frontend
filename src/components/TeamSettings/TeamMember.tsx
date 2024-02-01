b33b05ddec49163aee855395fdbdc1744670cdbf
mariyamfidha7 <133568286+mariyamfidha7@users.noreply.github.com>
2024-02-01 19:22:30 +0530

    test commit

--- src/components/TeamSettings/TeamMember.tsx
+++ src/components/TeamSettings/TeamMember.tsx
@@ -1,24 +1,62 @@
-import { Avatar, Button, ButtonGroup, ListItem, ListItemContent, ListItemDecorator, Typography } from '@mui/joy'
-import React from 'react'
+import {
+  Avatar,
+  Button,
+  ButtonGroup,
+  ListItem,
+  ListItemContent,
+  ListItemDecorator,
+  Typography,
+} from "@mui/joy";
+import React, { useState } from "react";
 
-const TeamMember = () => {
-  return (
-    <ListItem sx={{ flexDirection: { xs: 'column', md: 'row' }, alignItems: { xs: 'center', md: 'flex-start' } }}>    
-          <ListItemDecorator>
-            <Avatar src="/static/images/avatar/1.jpg" />
-          </ListItemDecorator>
-          <ListItemContent sx={{ ml: { xs: 0, md: 2 } }}>
-            <Typography level="title-sm" textColor={'black'}>Dencymol Baby</Typography>
-            <Typography level="body-xs" noWrap>
-              Project Manager
-            </Typography>
-          </ListItemContent>
-          <ButtonGroup sx={{ mt: { xs: 2, md: 0 } }} spacing="0.5rem" aria-label="spacing button group">
-            <Button disabled>Make Scrum Master</Button>
-            <Button>Remove</Button>
-          </ButtonGroup>
-        </ListItem>   
-  )
+interface TeamMemberProps {
+  member: {
+    id: number;
+    name: string;
+    role: string;
+    // Add more properties if needed
+  };
+  isScrumMaster: boolean;
+  onRemove: () => void;
+  onMakeScrumMaster: () => void;
 }
+
+const TeamMember: React.FC<TeamMemberProps> = ({
+  member,
+  isScrumMaster,
+  onRemove,
+  onMakeScrumMaster,
+}) => {
+  return (
+    <ListItem
+      sx={{
+        flexDirection: { xs: "column", md: "row" },
+        alignItems: { xs: "center", md: "flex-start" },
+      }}
+    >
+      <ListItemDecorator>
+        <Avatar src={`/static/images/avatar/${member.id}.jpg`} />
+      </ListItemDecorator>
+      <ListItemContent sx={{ ml: { xs: 0, md: 2 } }}>
+        <Typography level="title-sm" textColor={"black"}>
+          {member.name}
+        </Typography>
+        <Typography level="body-xs" noWrap>
+          {member.role}
+        </Typography>
+      </ListItemContent>
+      <ButtonGroup
+        sx={{ mt: { xs: 2, md: 0 } }}
+        spacing="0.5rem"
+        aria-label="spacing button group"
+      >
+        <Button disabled={isScrumMaster} onClick={onMakeScrumMaster}>
+          Make Scrum Master
+        </Button>
+        <Button onClick={onRemove}>Remove</Button>
+      </ButtonGroup>
+    </ListItem>
+  );
+};
 
 export default TeamMember;
