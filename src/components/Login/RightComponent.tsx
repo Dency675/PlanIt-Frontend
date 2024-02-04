df925b2e89e730a3f1782992a1152b8b05960e36
aljobiju <aljovadackel@gmail.com>
2024-02-04 17:06:54 +0530

    login ui done

---
+++ src/components/Login/RightComponent.tsx
@@ -0,0 +1,81 @@
+import { Divider, FormControl, FormLabel, Input, Link, Typography } from '@mui/joy'
+import Sheet from '@mui/joy/Sheet'
+import Box from '@mui/joy/Box'
+import React from 'react'
+import Button from '@mui/joy/Button';
+import Logo from '../../assets/images/logo_Pi.png'
+import { size } from '@floating-ui/core';
+import { width } from '@mui/system';
+import { Add } from '@mui/icons-material';
+import { SiMicrosoftoutlook } from "react-icons/si";
+
+
+const RightComponent = () => {
+  return (
+    <Sheet sx={{
+      width: 350,
+      // mx: 'auto', // margin left & right
+      marginLeft:0,
+      marginRight:'auto',
+      my: 12, // margin top & bottom
+      py: 3, // padding top & bottom
+      px: 2, // padding left & right
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 2,
+      borderRadius: 'sm',
+      boxShadow: 'md',
+    }}
+    variant="soft">
+       
+          <center>
+          <Box>
+          <img src={Logo} style={{ width: '100px', height: '45px'}}/>
+          </Box>
+          </center>
+          <Divider />
+          <center>
+          <div >
+          <Typography level="h4" component="h1" sx={{py:2, alignContent:'center',alignItems:'center'}}>
+            <b>Welcome!</b>
+          </Typography>
+          <Typography level="body-sm">Sign in to continue.</Typography>
+        </div>
+        </center>
+        {/* <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input
+            // html input attribute
+            name="email"
+            type="email"
+            placeholder="johndoe@email.com"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input
+            // html input attribute
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+        </FormControl> */}
+        <Button sx={{ mt: 1 /* margin top */ }} startDecorator={<SiMicrosoftoutlook />}> Continue with Outlook</Button>
+        <Divider />
+       
+        <Typography
+          endDecorator={<Link href="/sign-up">  Continue with Outlook</Link>}
+          fontSize="sm"
+          sx={{ alignSelf: 'center' }}
+        >
+          Don&apos;t have an account?
+        </Typography>
+      </Sheet>
+  )
+}
+
+export default RightComponent
+
+
+
+
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,15 +1,45 @@
-import { Button } from "@mui/joy";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import * as React from 'react';
+import CssBaseline from '@mui/joy/CssBaseline';
+import LeftComponent from '../../components/Login/LeftComponent';
+import RightComponent from '../../components/Login/RightComponent';
 
-const Login = () => {
-  // const navigate=useNavigate()
 
+
+
+
+// function ModeToggle() {
+//   const { mode, setMode } = useColorScheme();
+//   const [mounted, setMounted] = React.useState(false);
+
+//   // necessary for server-side rendering
+//   // because mode is undefined on the server
+//   React.useEffect(() => {
+//     setMounted(true);
+//   }, []);
+//   if (!mounted) {
+//     return <Button variant="soft">Change mode</Button>;
+//   }
+
+//   return (
+//     <Button
+//       variant="soft"
+//       onClick={() => {
+//         setMode(mode === 'light' ? 'dark' : 'light');
+//       }}
+//     >
+//       {mode === 'light' ? 'Turn dark' : 'Turn light'}
+//     </Button>
+//   );
+// }
+
+export default function Login() {
   return (
-    <div>
-      <Button>Login</Button>
-    </div>
+    <main  style={{ display: 'flex', justifyContent: 'center' }}>
+      {/* <ModeToggle /> */}
+      <CssBaseline />
+      <LeftComponent/>
+      <RightComponent/>
+     
+    </main>
   );
-};
-
-export default Login;
+}
