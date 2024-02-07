fa3e4c77227f634cc1d5f7db77c7f352d31a8d1f
aljobiju <aljovadackel@gmail.com>
2024-02-07 13:53:10 +0530

    userslist done

--- src/components/Admin/UsersList.tsx
+++ src/components/Admin/UsersList.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react';
 import { ColorPaletteProp } from '@mui/joy/styles';
 import Avatar from '@mui/joy/Avatar';
@@ -35,232 +34,91 @@ import AutorenewRoundedIcon from '@mui/icons-material/AutorenewRounded';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
+import { fetchUsersData } from '../../pages/Admin/apis/usersList';
+import { useState,useEffect } from 'react';
+import { UserList } from '../../pages/Admin/types/UserList';
+import {deleteUser} from '../../pages/Admin/apis/RemoveUser'
 
-const rows = [
-  {
-    // name: 'Olivia Ryhe',
-    email: 'olivia@email.com',
-    id: 'INV-123434567876543456788745677654567uyhgvcfrtyujnbvty78ij',
-    date: 'Feb 3, 2023',
-    status: 'Refunded',
-    customer: {
-      initial: 'O',
-     
-    },
-  },
-  {
-    id: 'INV-1233',
-    date: 'Feb 3, 2023',
-    status: 'Paid',
-    customer: {
-      initial: 'S',
-      name: 'Steve Hampton',
-      email: 'steve.hamp@email.com',
-    },
-  },
-  {
-    id: 'INV-1232',
-    date: 'Feb 3, 2023',
-    status: 'Refunded',
-    customer: {
-      initial: 'C',
-      name: 'Ciaran Murray',
-      email: 'ciaran.murray@email.com',
-    },
-  },
-  {
-    id: 'INV-1231',
-    date: 'Feb 3, 2023',
-    status: 'Refunded',
-    customer: {
-      initial: 'M',
-      name: 'Maria Macdonald',
-      email: 'maria.mc@email.com',
-    },
-  },
-  {
-    id: 'INV-1230',
-    date: 'Feb 3, 2023',
-    status: 'Cancelled',
-    customer: {
-      initial: 'C',
-      name: 'Charles Fulton',
-      email: 'fulton@email.com',
-    },
-  },
-  {
-    id: 'INV-1229',
-    date: 'Feb 3, 2023',
-    status: 'Cancelled',
-    customer: {
-      initial: 'J',
-      name: 'Jay Hooper',
-      email: 'hooper@email.com',
-    },
-  },
-  {
-    id: 'INV-1228',
-    date: 'Feb 3, 2023',
-    status: 'Refunded',
-    customer: {
-      initial: 'K',
-      name: 'Krystal Stevens',
-      email: 'k.stevens@email.com',
-    },
-  },
-  {
-    id: 'INV-1227',
-    date: 'Feb 3, 2023',
-    status: 'Paid',
-    customer: {
-      initial: 'S',
-      name: 'Sachin Flynn',
-      email: 's.flyn@email.com',
-    },
-  },
-  {
-    id: 'INV-1226',
-    date: 'Feb 3, 2023',
-    status: 'Cancelled',
-    customer: {
-      initial: 'B',
-      name: 'Bradley Rosales',
-      email: 'brad123@email.com',
-    },
-  },
-  {
-    id: 'INV-1225',
-    date: 'Feb 3, 2023',
-    status: 'Paid',
-    customer: {
-      initial: 'O',
-      name: 'Olivia Ryhe',
-      email: 'olivia@email.com',
-    },
-  },
-  {
-    id: 'INV-1224',
-    date: 'Feb 3, 2023',
-    status: 'Cancelled',
-    customer: {
-      initial: 'S',
-      name: 'Steve Hampton',
-      email: 'steve.hamp@email.com',
-    },
-  },
-  {
-    id: 'INV-1223',
-    date: 'Feb 3, 2023',
-    status: 'Paid',
-    customer: {
-      initial: 'C',
-      name: 'Ciaran Murray',
-      email: 'ciaran.murray@email.com',
-    },
-  },
-  {
-    id: 'INV-1221',
-    date: 'Feb 3, 2023',
-    status: 'Refunded',
-    customer: {
-      initial: 'M',
-      name: 'Maria Macdonald',
-      email: 'maria.mc@email.com',
-    },
-  },
-  {
-    id: 'INV-1220',
-    date: 'Feb 3, 2023',
-    status: 'Paid',
-    customer: {
-      initial: 'C',
-      name: 'Charles Fulton',
-      email: 'fulton@email.com',
-    },
-  },
-  {
-    id: 'INV-1219',
-    date: 'Feb 3, 2023',
-    status: 'Cancelled',
-    customer: {
-      initial: 'J',
-      name: 'Jay Hooper',
-      email: 'hooper@email.com',
-    },
-  },
-  {
-    id: 'INV-1218',
-    date: 'Feb 3, 2023',
-    status: 'Cancelled',
-    customer: {
-      initial: 'K',
-      name: 'Krystal Stevens',
-      email: 'k.stevens@email.com',
-    },
-  },
-  {
-    id: 'INV-1217',
-    date: 'Feb 3, 2023',
-    status: 'Paid',
-    customer: {
-      initial: 'S',
-      name: 'Sachin Flynn',
-      email: 's.flyn@email.com',
-    },
-  },
-  {
-    id: 'INV-1216',
-    date: 'Feb 3, 2023',
-    status: 'Cancelled',
-    customer: {
-      initial: 'B',
-      name: 'Bradley Rosales',
-      email: 'brad123@email.com',
-    },
-  },
-];
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = 'asc' | 'desc';
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key,
-): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string },
-) => number {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-// with exampleArray.slice().sort(exampleComparator)
-function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
+ function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
     }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+  type Order = 'asc' | 'desc';
+  
+  function getComparator<Key extends keyof any>(
+    order: Order,
+    orderBy: Key,
+  ): (
+    a: { [key in Key]: number | string },
+    b: { [key in Key]: number | string },
+  ) => number {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
 
-function RowMenu() {
+  function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
+    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+    stabilizedThis.sort((a, b) => {
+      const data = comparator(a[0], b[0]);
+      if (data !== 0) {
+        return data;
+      }
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+  
+
+
+export default function UsersList() {
+  const [order, setOrder] = React.useState<Order>('desc');
+  const [selected, setSelected] = React.useState<readonly string[]>([]);
+  const [open, setOpen] = React.useState(false);
+  // const [searchQuery, setSearchQuery] = React.useState('');
+  const [data, setData] = useState<UserList[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchedData = await fetchUsersData();
+        setData(fetchedData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    
+    fetchData();
+  }, []);
+
+
+
+  function RowMenu({ userId }: { userId: string }) {
+    
+    const handleDelete = async () => {
+      try {
+        await deleteUser(userId);
+        console.log(`User with ID ${userId} deleted successfully.`);
+        
+     
+      // setData(data.filter(user => user.id !== userId));
+      setData(data.map(user => user.id === userId ? {...user, status: 'inactive'} : user));
+      console.log("Member:", data);
+
+        
+      } catch (error) {
+        console.error('Error deleting user :', error);
+      }
+    };
+
+    
+
   return (
     <Dropdown>
       <MenuButton
@@ -274,72 +132,35 @@ function RowMenu() {
         <MenuItem>Rename</MenuItem>
         <MenuItem>Move</MenuItem>
         <Divider />
-        <MenuItem color="danger">Delete</MenuItem>
+        <MenuItem color="danger" onClick={handleDelete}>Delete</MenuItem>
       </Menu>
     </Dropdown>
   );
 }
 
-export default function OrderTable() {
-  const [order, setOrder] = React.useState<Order>('desc');
-  const [selected, setSelected] = React.useState<readonly string[]>([]);
-  const [open, setOpen] = React.useState(false);
-  const [searchQuery, setSearchQuery] = React.useState('');
+
+
 
   const handleSearch = (e: { target: { value: React.SetStateAction<string>; }; }) => {
     setSearchQuery(e.target.value);
   };
+  // const handleDeleteUser = (deletedUserId: string) => {
+  //   setData(data.filter(user => user.id !== deletedUserId));
+  // };
+  // const handleDeleteUser = (deletedUserId: string) => {
+  //   // Filter the data to remove the user with the deletedUserId
+  //   const updatedData = data.filter(user => user.id !== deletedUserId);
+  //   setData([...updatedData]);
+  // };
 
-  const filteredRows = rows.filter((row) =>
-    Object.values(row.customer).some((value) =>
+  const filteredRows = data.filter((row) =>
+    Object.values(row).some((value) =>
       value.toString().toLowerCase().includes(searchQuery.toLowerCase())
     )
   );
-  const renderFilters = () => (
-    // <React.Fragment>
-    //   <FormControl size="sm">
-    //     <FormLabel>Status</FormLabel>
-    //     <Select
-    //       size="sm"
-    //       placeholder="Filter by status"
-    //       slotProps={{ button: { sx: { whiteSpace: 'nowrap' } } }}
-    //     >
-    //       <Option value="paid">Paid</Option>
-    //       <Option value="pending">Pending</Option>
-    //       <Option value="refunded">Refunded</Option>
-    //       <Option value="cancelled">Cancelled</Option>
-    //     </Select>
-    //   </FormControl>
-    //   <FormControl size="sm">
-    //     <FormLabel>Category</FormLabel>
-    //     <Select size="sm" placeholder="All">
-    //       <Option value="all">All</Option>
-    //       <Option value="refund">Refund</Option>
-    //       <Option value="purchase">Purchase</Option>
-    //       <Option value="debit">Debit</Option>
-    //     </Select>
-    //   </FormControl>
-    //   <FormControl size="sm">
-    //     <FormLabel>Customer</FormLabel>
-    //     <Select size="sm" placeholder="All">
-    //       <Option value="all">All</Option>
-    //       <Option value="olivia">Olivia Rhye</Option>
-    //       <Option value="steve">Steve Hampton</Option>
-    //       <Option value="ciaran">Ciaran Murray</Option>
-    //       <Option value="marina">Marina Macdonald</Option>
-    //       <Option value="charles">Charles Fulton</Option>
-    //       <Option value="jay">Jay Hoper</Option>
-    //     </Select>
-    //   </FormControl>
-    // </React.Fragment>
-    <Button
-              color="primary"
-              startDecorator={<DownloadRoundedIcon />}
-              size="sm"
-            >
-              Download PDF
-            </Button>
-  );
+  // const renderFilters = () => (
+  
+  // );
   return (
     <React.Fragment>
       <Sheet
@@ -372,7 +193,7 @@ export default function OrderTable() {
             </Typography>
             <Divider sx={{ my: 2 }} />
             <Sheet sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-              {renderFilters()}
+              {/* {renderFilters()} */}
               <Button color="primary" onClick={() => setOpen(false)}>
                 Submit
               </Button>
@@ -380,7 +201,7 @@ export default function OrderTable() {
           </ModalDialog>
         </Modal>
       </Sheet>
-      <Box
+      <Box  
         className="SearchAndFilters-tabletUp"
         sx={{
           borderRadius: 'sm',
@@ -398,7 +219,14 @@ export default function OrderTable() {
           <Input  value={searchQuery}
         onChange={handleSearch} size="sm" placeholder="Search" startDecorator={<SearchIcon />} />
         </FormControl>
-        {renderFilters()}
+        <Button
+              color="primary"
+              startDecorator={<DownloadRoundedIcon />}
+              size="sm"
+            >
+              Download PDF
+            </Button>
+        {/* {renderFilters()} */}
       </Box>
       <Sheet
         className="OrderTableContainer"
@@ -430,23 +258,23 @@ export default function OrderTable() {
                 <Checkbox
                   size="sm"
                   indeterminate={
-                    selected.length > 0 && selected.length !== rows.length
+                    selected.length > 0 && selected.length !== data.length
                   }
-                  checked={selected.length === rows.length}
+                  checked={selected.length === data.length}
                   onChange={(event) => {
                     setSelected(
-                      event.target.checked ? rows.map((row) => row.id) : [],
+                      event.target.checked ? data.map((row) => row.id) : [],
                     );
                   }}
                   color={
-                    selected.length > 0 || selected.length === rows.length
+                    selected.length > 0 || selected.length === data.length
                       ? 'primary'
                       : undefined
                   }
                   sx={{ verticalAlign: 'text-bottom' }}
                 />
               </th>
-              <th style={{ width: 160, padding: '12px 6px' }}>
+              <th style={{ width: 120, padding: '12px 6px' }}>
                 <Link
                   underline="none"
                   color="primary"
@@ -462,19 +290,19 @@ export default function OrderTable() {
                     },
                   }}
                 >
-                  Emp Id
+                  User Id
                 </Link>
               </th>
-              <th style={{ width: 240, padding: '12px 6px' }}>Name</th>
+              <th style={{ width: 140, padding: '12px 6px' }}>Name</th>
               <th style={{ width: 140, padding: '12px 6px' }}>Status</th>
-              <th style={{ width: 140, padding: '12px 6px' }}>Department</th>
+              <th style={{ width: 240, padding: '12px 6px' }}>Department</th>
               <th style={{ width: 140, padding: '12px 6px' }}> </th>
-            </tr> 
+            </tr>
           </thead>
           <tbody>
-            {/* {stableSort(rows, getComparator(order, 'id')).map((row) => (
-              <tr key={row.id}> */}
-                {stableSort(filteredRows, getComparator(order, 'id')).map((row) => (
+            {stableSort(filteredRows, getComparator(order, 'id')).map((row) => (
+              
+              //  {filteredRows.map((row) => (
               <tr key={row.id}>
                 <td style={{ textAlign: 'center', width: 120 }}>
                   <Checkbox
@@ -497,10 +325,10 @@ export default function OrderTable() {
                 </td>
                 <td>
                 <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
-                    <Avatar size="sm">{row.customer.initial}</Avatar>
+                    {/* <Avatar size="sm">{row.customer.initial}</Avatar> */}
                     <div>
-                      <Typography level="body-xs">{row.customer.name}</Typography>
-                      <Typography level="body-xs">{row.customer.email}</Typography>
+                      <Typography level="body-xs">{row.givenName}</Typography>
+                      <Typography level="body-xs">{row.email}</Typography>
                     </div>
                   </Box>
                  
@@ -511,16 +339,16 @@ export default function OrderTable() {
                     size="sm"
                     startDecorator={
                       {
-                        Paid: <CheckRoundedIcon />,
-                        Refunded: <AutorenewRoundedIcon />,
-                        Cancelled: <BlockIcon />,
+                        active: <CheckRoundedIcon />,
+                        // inactive: <AutorenewRoundedIcon />,
+                        inactive: <BlockIcon />,
                       }[row.status]
                     }
                     color={
                       {
-                        Paid: 'success',
-                        Refunded: 'neutral',
-                        Cancelled: 'danger',
+                        active: 'success',
+                        // inactive: 'neutral',
+                        inactive: 'danger',
                       }[row.status] as ColorPaletteProp
                     }
                   >
@@ -528,14 +356,15 @@ export default function OrderTable() {
                   </Chip>
                 </td>
                 <td>
-                <Typography level="body-xs">{row.date}</Typography>
+                <Typography level="body-xs">{row.department}</Typography>
                 </td>
                 <td>
                   <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
                     <Link level="body-xs" component="button">
                       Download
                     </Link>
-                    <RowMenu />
+                    <RowMenu userId={row.id}  />
+
                   </Box>
                 </td>
               </tr>
--- src/pages/Admin/apis/usersList.tsx
+++ src/pages/Admin/apis/usersList.tsx
@@ -1,12 +1,10 @@
-
-import axios from 'axios';
-
-export const fetchUserDetails = async () => {
+import axios from "axios";
+ 
+export const fetchUsersData = async () => {
   try {
-    const response = await axios.get('https:localhost:3000/getUser');
-    return response.data; // Assuming the response contains an array of user objects
+    const response = await axios.get(`http://localhost:3001/getAllUsers`);
+    return response.data.data;
   } catch (error) {
-    console.error('Error fetching user details:', error);
-    return []; // Return an empty array if there's an error
+    console.error("Error fetching active count:", error);
   }
 };
