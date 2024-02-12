5aaa926a6ffedcc3232c7e620f2c144e8060528d
aljobiju <aljovadackel@gmail.com>
2024-02-12 11:02:58 +0530

    search user integration done

---
+++ src/pages/Admin/apis/SearchUser.tsx
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+// Function to search users
+export const searchUsers = async (query:any) => {
+  try {
+    const response = await axios.get(`http://localhost:3001/searchUser?search=${query}`);
+    return response.data.userResults;
+  } catch (error) {
+    console.error('Error searching users:', error);
+    throw error;
+  }
+};
