e87d78eeb914c357504c1164000b3171a624208a
geevarghese2000 <geevarghesemvarghese@gmail.com>
2024-02-13 22:43:33 +0530

    resolved

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import ReportPage from "./pages/ReportPage/ReportPage";
 import TeamSettings from "./pages/TeamSettings/TeamSettings";
 import Header from "./components/Navbar/Header";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login/Login";
 import RoomCreation from "./pages/RoomCreation/RoomCreation";
 import { Box } from "@mui/joy";
-import VotingRoom from "./pages/VotingRoom/VotingRoom";
 
 import {
   experimental_extendTheme as materialExtendTheme,
@@ -16,7 +14,6 @@ import {
 } from "@mui/material/styles";
 import { CssVarsProvider as JoyCssVarsProvider } from "@mui/joy/styles";
 import CssBaseline from "@mui/joy/CssBaseline";
-import TeamManagement from "./pages/TeamManagement/TeamManagement";
 import AppRouter from "./Routes";
 import Admin from "./pages/Admin/Admin";
 import Users from "./pages/Admin/Admin";
