d2a262bfe79f4d772ca6c7783f9c4dbd087e8525
Dency675 <neelamkavil2002@gmail.com>
2024-02-13 23:05:15 +0530

    corrected alignment of team management page

--- src/components/Admin/Sidebar.tsx
+++ src/components/Admin/Sidebar.tsx
@@ -1,41 +1,42 @@
-import React, { useState } from 'react';
-import GlobalStyles from '@mui/joy/GlobalStyles';
-import Avatar from '@mui/joy/Avatar';
-import Box from '@mui/joy/Box';
-import Button from '@mui/joy/Button';
-import Card from '@mui/joy/Card';
-import Chip from '@mui/joy/Chip';
-import Divider from '@mui/joy/Divider';
-import IconButton from '@mui/joy/IconButton';
-import Input from '@mui/joy/Input';
-import LinearProgress from '@mui/joy/LinearProgress';
-import List from '@mui/joy/List';
-import ListItem from '@mui/joy/ListItem';
-import ListItemButton, { listItemButtonClasses } from '@mui/joy/ListItemButton';
-import ListItemContent from '@mui/joy/ListItemContent';
-import Typography from '@mui/joy/Typography';
-import Sheet from '@mui/joy/Sheet';
-import Stack from '@mui/joy/Stack';
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
-import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
-import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
-import QuestionAnswerRoundedIcon from '@mui/icons-material/QuestionAnswerRounded';
-import GroupRoundedIcon from '@mui/icons-material/GroupRounded';
-import SupportRoundedIcon from '@mui/icons-material/SupportRounded';
-import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
-import BrightnessAutoRoundedIcon from '@mui/icons-material/BrightnessAutoRounded';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import logo from '../../assets/images/logo_Pi.png'
-import ColorSchemeToggle from './ColorSchemeToggle';
-import { closeSidebar } from '../utils';
-import GroupIcon from '@mui/icons-material/Group';
-import Groups2Icon from '@mui/icons-material/Groups2';
-import UsersList from './UsersList';
-import TeamsList from './TeamsList';
+import React, { useState } from "react";
+import GlobalStyles from "@mui/joy/GlobalStyles";
+import Avatar from "@mui/joy/Avatar";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Card from "@mui/joy/Card";
+import Chip from "@mui/joy/Chip";
+import Divider from "@mui/joy/Divider";
+import IconButton from "@mui/joy/IconButton";
+import Input from "@mui/joy/Input";
+import LinearProgress from "@mui/joy/LinearProgress";
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import ListItemButton, { listItemButtonClasses } from "@mui/joy/ListItemButton";
+import ListItemContent from "@mui/joy/ListItemContent";
+import Typography from "@mui/joy/Typography";
+import Sheet from "@mui/joy/Sheet";
+import Stack from "@mui/joy/Stack";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import AssignmentRoundedIcon from "@mui/icons-material/AssignmentRounded";
+import QuestionAnswerRoundedIcon from "@mui/icons-material/QuestionAnswerRounded";
+import GroupRoundedIcon from "@mui/icons-material/GroupRounded";
+import SupportRoundedIcon from "@mui/icons-material/SupportRounded";
+import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+import BrightnessAutoRoundedIcon from "@mui/icons-material/BrightnessAutoRounded";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import logo from "../../assets/images/logo_Pi.png";
+import ColorSchemeToggle from "./ColorSchemeToggle";
+import { closeSidebar } from "../utils";
+import GroupIcon from "@mui/icons-material/Group";
+import Groups2Icon from "@mui/icons-material/Groups2";
+import UsersList from "./UsersList";
+import TeamsList from "./TeamsList";
+import { useMsal } from "@azure/msal-react";
 
 function Toggler({
   defaultExpanded = false,
@@ -55,11 +56,11 @@ function Toggler({
       {renderToggle({ open, setOpen })}
       <Box
         sx={{
-          display: 'grid',
-          gridTemplateRows: open ? '1fr' : '0fr',
-          transition: '0.2s ease',
-          '& > *': {
-            overflow: 'hidden',
+          display: "grid",
+          gridTemplateRows: open ? "1fr" : "0fr",
+          transition: "0.2s ease",
+          "& > *": {
+            overflow: "hidden",
           },
         }}
       >
@@ -75,42 +76,54 @@ interface SidebarProps {
 
 export const Sidebar: React.FC<SidebarProps> = ({ SendValueToParent }) => {
   const [selectedTab, setSelectedTab] = useState(null);
-//  console.log(selectedTab)
+  //  console.log(selectedTab)
 
-  const handleTabClick = (tabName:any) => {
+  const handleTabClick = (tabName: any) => {
     setSelectedTab(tabName);
     SendValueToParent(tabName);
+  };
+
+  const { instance } = useMsal();
+
+  const logOut = () => {
+    localStorage.removeItem("roleID");
+    sessionStorage.clear();
+    localStorage.removeItem("userId");
+    instance.logoutPopup({
+      postLogoutRedirectUri: "/",
+      mainWindowRedirectUri: "/",
+    });
   };
 
   return (
     <Sheet
       className="Sidebar"
       sx={{
-        position: { xs: 'fixed', md: 'sticky' },
+        position: { xs: "fixed", md: "sticky" },
         transform: {
-          xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1)))',
-          md: 'none',
+          xs: "translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1)))",
+          md: "none",
         },
-        transition: 'transform 0.4s, width 0.4s',
+        transition: "transform 0.4s, width 0.4s",
         zIndex: 10000,
-        height: '100dvh',
-        width: 'var(--Sidebar-width)',
+        height: "100dvh",
+        width: "var(--Sidebar-width)",
         top: 0,
         p: 2,
         flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
+        display: "flex",
+        flexDirection: "column",
         gap: 2,
-        borderRight: '1px solid',
-        borderColor: 'divider',
+        borderRight: "1px solid",
+        borderColor: "divider",
       }}
     >
       <GlobalStyles
         styles={(theme) => ({
-          ':root': {
-            '--Sidebar-width': '220px',
-            [theme.breakpoints.up('lg')]: {
-              '--Sidebar-width': '240px',
+          ":root": {
+            "--Sidebar-width": "220px",
+            [theme.breakpoints.up("lg")]: {
+              "--Sidebar-width": "240px",
             },
           },
         })}
@@ -118,34 +131,34 @@ export const Sidebar: React.FC<SidebarProps> = ({ SendValueToParent }) => {
       <Box
         className="Sidebar-overlay"
         sx={{
-          position: 'fixed',
+          position: "fixed",
           zIndex: 9998,
           top: 0,
           left: 0,
-          width: '100vw',
-          height: '100vh',
-          opacity: 'var(--SideNavigation-slideIn)',
-          backgroundColor: 'var(--joy-palette-background-backdrop)',
-          transition: 'opacity 0.4s',
+          width: "100vw",
+          height: "100vh",
+          opacity: "var(--SideNavigation-slideIn)",
+          backgroundColor: "var(--joy-palette-background-backdrop)",
+          transition: "opacity 0.4s",
           transform: {
-            xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--Sidebar-width, 0px)))',
-            lg: 'translateX(-100%)',
+            xs: "translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--Sidebar-width, 0px)))",
+            lg: "translateX(-100%)",
           },
         }}
         onClick={() => closeSidebar()}
       />
-      <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+      <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
         <Box>
-          <img src={logo} style={{ width: '100px', height: '45px'}}/>
+          <img src={logo} style={{ width: "100px", height: "45px" }} />
         </Box>
       </Box>
       <Box
         sx={{
           minHeight: 0,
-          overflow: 'hidden auto',
+          overflow: "hidden auto",
           flexGrow: 1,
-          display: 'flex',
-          flexDirection: 'column',
+          display: "flex",
+          flexDirection: "column",
           [`& .${listItemButtonClasses.root}`]: {
             gap: 1.5,
           },
@@ -155,12 +168,15 @@ export const Sidebar: React.FC<SidebarProps> = ({ SendValueToParent }) => {
           size="sm"
           sx={{
             gap: 1,
-            '--List-nestedInsetStart': '30px',
-            '--ListItem-radius': (theme) => theme.vars.radius.sm,
+            "--List-nestedInsetStart": "30px",
+            "--ListItem-radius": (theme) => theme.vars.radius.sm,
           }}
         >
           <ListItem>
-            <ListItemButton selected={selectedTab === 'users'} onClick={() => handleTabClick('users')}>
+            <ListItemButton
+              selected={selectedTab === "users"}
+              onClick={() => handleTabClick("users")}
+            >
               <GroupIcon />
               <ListItemContent>
                 <Typography level="title-sm">Users</Typography>
@@ -169,7 +185,10 @@ export const Sidebar: React.FC<SidebarProps> = ({ SendValueToParent }) => {
           </ListItem>
 
           <ListItem>
-            <ListItemButton selected={selectedTab === 'teams'} onClick={() => handleTabClick('teams')}>
+            <ListItemButton
+              selected={selectedTab === "teams"}
+              onClick={() => handleTabClick("teams")}
+            >
               <Groups2Icon />
               <ListItemContent>
                 <Typography level="title-sm">Teams</Typography>
@@ -218,10 +237,10 @@ export const Sidebar: React.FC<SidebarProps> = ({ SendValueToParent }) => {
           </ListItem> */}
           {/* {selectedTab === 'users' && <UsersList/> }
       {selectedTab === 'teams' && <TeamsList />} */}
-      {/* {selectedTab === 'orders' && <OrdersTabContent />} */}
+          {/* {selectedTab === 'orders' && <OrdersTabContent />} */}
         </List>
       </Box>
-      <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+      <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
         <Avatar
           variant="outlined"
           size="sm"
@@ -231,14 +250,13 @@ export const Sidebar: React.FC<SidebarProps> = ({ SendValueToParent }) => {
           <Typography level="title-sm">Admin</Typography>
           <Typography level="body-xs">Ad@experionglobal.com</Typography>
         </Box>
-        <IconButton size="sm" variant="plain" color="neutral">
+        <IconButton size="sm" variant="plain" color="neutral" onClick={logOut}>
           <LogoutRoundedIcon />
         </IconButton>
       </Box>
     </Sheet>
   );
-}
+};
 function SendValueToParent(tabName: any) {
-  throw new Error('Function not implemented.');
+  throw new Error("Function not implemented.");
 }
-
