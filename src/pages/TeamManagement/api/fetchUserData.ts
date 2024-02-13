0af3ed44393cd049e2fa94a526f8d8f14e21d9f8
Hariprasad-j21 <hariprasad.j21@gmail.com>
2024-02-13 14:18:45 +0530

    conflict resolved

--- src/pages/TeamManagement/api/fetchUserData.ts
+++ src/pages/TeamManagement/api/fetchUserData.ts
@@ -3,18 +3,24 @@ import axios, { AxiosResponse } from "axios";
 interface UserData {
   id: string;
   givenName: string;
+  email: string;
   // Add other properties if needed
 }
 
-const getUserInformationById = async (id: string): Promise<string> => {
+const getUserInformationById = async (
+  id: string
+): Promise<{ givenName: string; email: string }> => {
   try {
     const response: AxiosResponse<{ message: string; data: UserData }> =
       await axios.get(`http://localhost:3001/getUserInformationById?id=${id}`);
     const userData = response.data.data;
-    return userData.givenName;
+
+    const { givenName, email } = userData;
+    console.log("userdata", userData);
+    return { givenName, email };
   } catch (error) {
     console.error("Error fetching user information:", error);
-    return ""; // Return an empty string or handle error as needed
+    return { givenName: "", email: "" }; // Return an object with default values or handle error as needed
   }
 };
 
