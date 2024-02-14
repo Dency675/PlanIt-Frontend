74201bb38793343eac298a5506d8eb1c0d40f5d4
Hariprasad-j21 <hariprasad.j21@gmail.com>
2024-02-14 12:47:28 +0530

    added profile modal and room creation validation

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,21 +1,61 @@
 import { Grid, Avatar, Typography, Paper, Box } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
-const ProfilePage = () => {
-  const avatarUrl = "your_avatar_url.jpg";
-  const userName = "Your Name";
-  const userEmail = "your.email@example.com";
+interface UserData {
+  id: string;
+  employeeId: string;
+  name: string;
+  email: string;
+}
+
+interface ProfilePageProps {
+  user?: UserData; // Change the prop to accept the entire user object
+}
+
+const ProfilePage: React.FC<ProfilePageProps> = ({ user }) => {
+  // You can add additional state to hold user information
+  const [userData, setUserData] = React.useState<any>(null);
+
+  console.log("user", user);
+
+  // useEffect(() => {
+  //   const fetchUserData = async () => {
+  //     try {
+  //       const response = await axios.get(`http://localhost:3001/getUserById`, {
+  //         params: {
+  //           userId: userId,
+  //         },
+  //       });
+
+  //       setUserData(response.data);
+  //     } catch (error) {
+  //       console.error("Error fetching user data:", error);
+  //     }
+  //   };
+
+  //   fetchUserData();
+  // }, [userId]);
+
+  const avatarUrl = user?.name[0] || "default_avatar_url.jpg";
+  const userName = user?.name || "Unknown User";
+  const userEmail = user?.email || "unknown.email@example.com";
+  const employeeId = user?.employeeId || "Unknown";
 
   return (
     <Grid
       container
       justifyContent="center"
       alignItems="center"
-      style={{ height: "100vh", background: "#f5f5f5" }}
+      style={{ height: "100vh" }}
     >
       <Paper
         elevation={3}
-        style={{ padding: "40px", textAlign: "center", maxWidth: "500px" }}
+        style={{
+          padding: "40px",
+          textAlign: "center",
+          maxWidth: "500px",
+        }}
       >
         <Typography variant="h4" gutterBottom>
           Profile
@@ -37,6 +77,10 @@ const ProfilePage = () => {
         <Box mt={2}>
           <Typography variant="h6">Email:</Typography>
           <Typography>{userEmail}</Typography>
+        </Box>
+        <Box>
+          <Typography variant="h6">Employee Id:</Typography>
+          <Typography>{employeeId}</Typography>
         </Box>
       </Paper>
     </Grid>
