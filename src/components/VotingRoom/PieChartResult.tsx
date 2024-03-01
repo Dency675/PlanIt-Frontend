1086be20a9e0389572e4f00eb5e5de0bd7664887
Dency675 <neelamkavil2002@gmail.com>
2024-03-01 10:09:39 +0530

    corrected the status change of participants in voting room

--- src/components/VotingRoom/PieChartResult.tsx
+++ src/components/VotingRoom/PieChartResult.tsx
@@ -11,7 +11,7 @@ const PieChartResult: React.FC<tablePropType> = ({ scoreCounts, score }) => {
   const data = Object.keys(scoreCounts).map((key) => ({
     id: key,
     value: scoreCounts[key],
-    label: `Story Point  ${score[key]}`.toString(),
+    label: `${score[key]}`.toString(),
   }));
 
   console.log("scorescore", score);
@@ -27,11 +27,20 @@ const PieChartResult: React.FC<tablePropType> = ({ scoreCounts, score }) => {
           data,
           highlightScope: { faded: "global", highlighted: "item" },
           faded: { innerRadius: 30, additionalRadius: -30, color: "grey" },
+          cx: 100,
         },
       ]}
-      height={150}
-      width={400}
-      sx={{ alignItems: "center" }}
+      height={180}
+      width={250}
+      style={{ alignItems: "start" }}
+      slotProps={{
+        legend: {
+          direction: "column",
+          position: { vertical: "middle", horizontal: "right" },
+          padding: 0,
+          // hidden: true,
+        },
+      }}
     />
   );
 };
